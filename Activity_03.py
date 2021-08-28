a50e6408012519c8b9f7d7d60e9127c5d06f82e0
BhargavSPatil <89650634+BhargavSPatil@users.noreply.github.com>
2021-08-28 13:17:11 +0530

    Update Activity_03.py

---
+++ Activity_03.py
@@ -0,0 +1,10 @@
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+concatenated_str = str1 + str2
+print(concatenated_str)
+print(5*concatenated_str)
+
+#to print with space between each concatenated string
+
+concatenated_str = str1 + str2 + " "
+print(5*concatenated_str)
