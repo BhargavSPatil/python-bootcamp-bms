b670690f7ff77384a7eb7f0b2a7217d788fa0dbb
BhargavSPatil <89650634+BhargavSPatil@users.noreply.github.com>
2021-08-29 17:28:39 +0530

    Update Activity_13.py

--- Activity_13.py
+++ Activity_13.py
@@ -1,5 +1,15 @@
-m = int(input("Enter the start number: "))
-n = int(input("Enter the stop number: "))
-for i in range(m, n+1):
-    if(i%2 != 0):
-        print(i, end=",")
+def inputs():
+    m = int(input("Enter the start number: "))
+    n = int(input("Enter the stop number: "))
+    return m, n
+
+def odd_num(m, n):
+    for i in range(m, n+1):
+        if(i%2 != 0):
+            print(i, end=",")
+
+def main():
+    m, n = inputs()
+    odd_num(m, n)
+
+main()
