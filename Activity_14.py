cf5457fbc103bf09a4d1bb8398ca54c0dcbe986d
BhargavSPatil <89650634+BhargavSPatil@users.noreply.github.com>
2021-08-29 18:47:41 +0530

    Update Activity_14.py

--- Activity_14.py
+++ Activity_14.py
@@ -1,11 +1,24 @@
-num = int(input("Enter a number: "))
-flag = 0
-for i in range(2, int((num/2)+1)):
-    if(num%i == 0):
-        flag = 1
-        break
+def inputs():
+    num = int(input("Enter a number: "))
+    return num
 
-if(flag == 0):
-    print("The number entered is a prime number")
-else:
-    print("The number entered is not a prime number")
+def prime_num(num):
+    flag = 0    
+    for i in range(2, int((num/2)+1)):
+        if(num%i == 0):
+            flag = 1
+            break
+    return flag
+
+def display(flag, num):
+    if(flag == 0):
+        print(num, " is a prime number")
+    else:
+        print(num, " is not a prime number")
+
+def main():
+    num = inputs()
+    flag = prime_num(num)
+    display(flag, num)
+
+main()
