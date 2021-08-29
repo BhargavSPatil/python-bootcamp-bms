207d5750e8342d4015116adc117178b574cc4809
BhargavSPatil <89650634+BhargavSPatil@users.noreply.github.com>
2021-08-29 17:15:12 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -11,3 +11,6 @@ num1, num2, num3 = map(int, input("Enter the three numbers: "). split())
 large_num = compare(num1, num2, num3)
 
 print("%d is the greatest number among %d, %d and %d" %(large_num, num1, num2, num3))
+
+def great_ter(a, b, c):
+    print(a) if a>b and a>c else print(b) if b>c and b>a else print(c)
